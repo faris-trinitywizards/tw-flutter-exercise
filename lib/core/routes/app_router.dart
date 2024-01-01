ae70a4187db3d2c9850295fe4c01012fa15b3085
faris-trinitywizards <faris.g@trinitywizards.com>
2024-01-02 05:16:47 +0700

    create home page

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -5,6 +5,7 @@ import 'package:trinity_lecture_app/core/routes/app_router.gr.dart';
 class AppRouter extends $AppRouter {
   @override
   List<AutoRoute> routes = [
-    AutoRoute(page: SplashRoute.page, initial: true),
+    // AutoRoute(page: SplashRoute.page, initial: true),
+    AutoRoute(page: HomeRoute.page, initial: true),
   ];
 }
---
+++ lib/presentation/pages/home/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:auto_route/annotations.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:trinity_lecture_app/presentation/widgets/molecules/action_text.dart';
+
+@RoutePage()
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              ActionText(
+                onTap: () {},
+                title: "Dummy UI",
+                desc: "Introduction basic flutter widget ui",
+              ),
+              ActionText(
+                onTap: () {},
+                title: "Counter Example (State Management)",
+                desc: "Introduction of state management using flutter_bloc. Level: 1",
+              ),
+              ActionText(
+                onTap: () {},
+                title: "Input Validations Example",
+                desc:
+                    "Flutter state management using flutter_bloc to handle validation in text field. Level 2",
+              ),
+              ActionText(
+                onTap: () {},
+                title: "Calculator Example (State Management)",
+                desc:
+                    "Flutter state management using flutter_bloc to calculate simple syntax. Level: 3",
+              ),
+              ActionText(
+                onTap: () {},
+                title: "News App",
+                desc: "Api calling using free source from NYTimes",
+              ),
+              ActionText(
+                onTap: () {},
+                title: "To Do App",
+                desc:
+                    "Create a to do list that saved in local storage using hydrated_bloc: Level: 4",
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
